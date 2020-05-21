bca8258a3b88d6c884e7917c007a601c9b380fe7
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2020-05-21 20:03:49 -0300

    Owner

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "utils/colors.c"
+#include "models/owner/owner.c"
 
 int main() {
+    struct owner _owner;
     printf( CYAN "You mom Gaaay" RESET_COLOR);
+    readOwner(_owner).
+    printf("%s", _owner.nome);
 }
--- models/owner/owner.c
+++ models/owner/owner.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
+#include "../address/address.c"
 
 struct owner {
-    char nome[30];
+    char nome[80];
+    char CPF[15];
+    int qntd_de_casas;
+    struct address saddress;
 };
 
 void readOwner(struct owner _owener) {
