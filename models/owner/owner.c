24e26f7b4731d5d3d4ff6647919be1081b48192f
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2020-05-21 20:25:29 -0300

    Teste

--- models/owner/owner.c
+++ models/owner/owner.c
@@ -13,3 +13,9 @@ void readOwner(struct owner *_owner) {
     printf("informe o nome: ");
     gets(_owner->nome);
 }
+
+void aloca_owner(struct owner **p_owner,int tam){
+    if((*p_owner = (struct owner *)realloc(*p_owner, tam*sizeof(struct owner))) == NULL){
+        printf("\nNao contem espaço vago.");
+    }
+}
