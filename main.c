8d836db251e9cf1c85a5f904ad8319c867a99859
Luis Otavio Martins <luisotaviomgr1@gmail.com>
2020-05-21 21:30:53 -0300

    No errors

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "utils/colors.c"
 #include "models/owner/owner.c"
-#include "models/address/OwnerAddress.c"
 
 int main() {
-    struct owner *_owner = NULL;
-    aloca_owner(&_owner, 1);
-    printf("oi");
+    
 }
--- models/address/OwnerAddress.c
+++ models/address/OwnerAddress.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <string.h>
-#include "../../utils/colors.c"
 #include <stdio.h>
 
 struct address{
@@ -13,28 +12,3 @@ struct address{
     char cel[15];
     char email[30];
 };
-
-struct address stdReadAddress() {
-    struct address _address;
-    printf("informe o logradouro: "); 
-    gets(_address.logradouro);
-    printf("inform o bairro: ");
-    gets(_address.bairro);
-    printf("informe o CEP: ");
-    gets(_address.CEP);
-    printf("informe a cidade: ");
-    gets(_address.cidade);
-    printf("informe o estado: ");
-    gets(_address.estado);
-    printf("informe o fone: ");
-    gets(_address.fone);
-    printf("informe o cel: ");
-    gets(_address.cel);
-    printf("iforme o email: ");
-    gets(_address.email);
-    return _address;
-}
-
-void stdWriteAddress() {
-
-}
