2196bebe7729e251f1f31d717ebb969320e57066
Gustavo Henrique Coimbra Galo <gustavo.henrique@helpper.com.br>
2020-06-10 17:54:04 -0300

    fuck the message

--- main.c
+++ main.c
@@ -5,10 +5,31 @@
 
 #define FILE_NAME "imovel.bin"
 
+void writeTest(struct test _test);
 struct test {
 	char name[30];
 };
 
 int main() {
 
+}
+
+void writeTest(struct test _test) {
+	FILE *file = fopen(FILE_NAME, "ab");
+	if (file == NULL) {
+		printf("\nErro ao abrir o arquivo.");
+	}
+	else {
+		fwrite(&_test, sizeof(struct test), 1, file);
+	}
+}
+
+void readAllTest() {
+	FILE *file = fopen(FILE_NAME, "rb");
+	if (file == NULL) {
+		printf("\nErro ao abrir o arquivo.");
+	}
+	else {
+		
+	}
 }
